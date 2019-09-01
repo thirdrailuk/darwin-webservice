1ef9702b034ad0220194f5062acf53091ca5ffde
Ben McManus <bennoislost@users.noreply.github.com>
2019-09-01 14:08:38 +0100

    Update reference script

--- scripts/reference/reason-code.php
+++ scripts/reference/reason-code.php
@@ -6,6 +6,8 @@ require __DIR__ . '/../include.php';
 
 try {
     $result = ClientFactory::create(openldbs_token())->getReasonCode(501);
+
+    var_dump($result);
 }
 catch (\Exception $e) {
     echo $e->getMessage() . PHP_EOL;
