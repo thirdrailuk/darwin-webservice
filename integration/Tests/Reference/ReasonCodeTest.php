0c8f5a86c803c3fbecfd64ac2ba75f09f05841a8
Ben McManus <bennoislost@users.noreply.github.com>
2019-09-02 07:49:28 +0100

    Update ReasonCode integration test

--- integration/Tests/Reference/ReasonCodeTest.php
+++ integration/Tests/Reference/ReasonCodeTest.php
@@ -3,19 +3,18 @@
 namespace Tests\Reference;
 
 use PHPUnit\Framework\TestCase;
+use TrainjunkiesPackages\DarwinWebservice\Soap\Response\ReasonCode;
 
 class ReasonCodeTest extends TestCase
 {
     public function test_it_can_get_details_for_a_reason_code()
     {
+        /** @var ReasonCode $reasonCode */
         $reasonCode = clientFactory()->getReasonCode(501);
 
-        $this->assertObjectHasAttribute('GetReasonCodeResult', $reasonCode);
-        $this->assertObjectHasAttribute('code', $reasonCode->GetReasonCodeResult);
-        $this->assertObjectHasAttribute('lateReason', $reasonCode->GetReasonCodeResult);
-        $this->assertObjectHasAttribute('cancReason', $reasonCode->GetReasonCodeResult);
-
-        $this->assertEquals(501, $reasonCode->GetReasonCodeResult->code);
+        $this->assertEquals(501, $reasonCode->code());
+        $this->assertNotEmpty($reasonCode->cancelledReason());
+        $this->assertNotEmpty($reasonCode->lateReason());
     }
 
     function test_it_will_throw_soapFault_when_reason_code_not_found()
