37583d1ba1a1e02f5e33449b4d43735397d5ee93
Ben McManus <bennoislost@users.noreply.github.com>
2019-09-30 14:49:41 +0100

    Add integration test for departure board

---
+++ integration/Tests/Staff/GetDepartureBoardWithDetailsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Reference;
+
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use TrainjunkiesPackages\DarwinWebservice\ClientFactory;
+use TrainjunkiesPackages\DarwinWebservice\Collection;
+use TrainjunkiesPackages\DarwinWebservice\Soap\Response\GetBoardWithDetailsResult;
+use TrainjunkiesPackages\DarwinWebservice\Soap\Response\TrainService;
+
+class GetDepartureBoardWithDetailsTest extends TestCase
+{
+    public function test_it_can_get_departures_for_location()
+    {
+        /** @var GetBoardWithDetailsResult $result */
+        $result = clientFactory()->getDepartureBoardWithDetails(
+            10,
+            'MAC',
+            (new DateTimeImmutable('now'))
+        );
+
+        /** @var Collection $trainServices */
+        $trainServices = $result->trainServices();
+
+        $this->assertEquals($result->crs(), 'MAC');
+        $this->assertEquals($result->locationName(), 'Macclesfield');
+        $this->assertInstanceOf(Collection::class, $result->trainServices());
+
+        if ($trainServices->count() > 0) {
+            /** @var TrainService $service */
+            $service = $trainServices[0];
+
+            $this->assertNotEmpty($service->rid());
+            $this->assertNotEmpty($service->uid());
+            $this->assertNotEmpty($service->originCrs());
+            $this->assertNotEmpty($service->destinationCrs());
+        }
+    }
+}
